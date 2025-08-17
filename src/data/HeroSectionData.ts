cc53b2c776e4bbdbadbc54449cd95e7a857c957d
Muzamil Ahmad <66633369+Muzamil-07@users.noreply.github.com>
2025-08-17 14:23:18 +0500

    delete unused files

--- src/data/HeroSectionData.ts
+++
@@ -1,9 +0,0 @@
-interface hero {
-  text: string;
-  buttonText: string;
-}
-
-export const heroData: hero = {
-  text: "\u00A0Build, Rent, Consume \n AI from Anyway",
-  buttonText: "Launch Now",
-};
--- src/data/imageShowCaseData.ts
+++
@@ -1,9 +0,0 @@
-interface ImageShowCaseData {
-    gradientImage: string;
-    image: string;
-}
-
-export const imageShowCaseData: ImageShowCaseData = {
-    gradientImage: '/gradient.png',
-    image: '/showCase.png',
-}
